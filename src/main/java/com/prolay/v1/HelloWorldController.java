d7922b8a2b44d1bebd94e7376e5955b4398f8b01
Dutta <702839@cognizant.com>
2021-08-08 16:42:59 +0530

    Last commit

--- src/main/java/com/prolay/v1/HelloWorldController.java
+++ src/main/java/com/prolay/v1/HelloWorldController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World V1";
+		return "Hello World Latest";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World Bean V1");
+		return new HelloWorldBean("Hello World Bean Latest");
 	}
 
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-		return new HelloWorldBean(String.format("Hello World V1, %s", name));
+		return new HelloWorldBean(String.format("Hello World Latest, %s", name));
 	}
 }
